0ef1964993f701154e7e0098bd920b0b3b27dac2
aclar <a.clarke0307@gmail.com>
2021-07-15 21:15:27 -0400

    variables for id and arrays

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+// variables to to ids in html
+var startTimeEl = document.querySelector("#startTime");
+var beginEl = document.querySelector("#begin");
+var questionsEl = document.querySelector("#questions");
+var containerEl = document.querySelector("#container");
+//var for an array for options
+var options = [
+    {
+        title: "Commonly used data types DO NOT include",
+        choices:["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts",
+    },
+    {
+        title: "The condition in an if/else statement is enclosed within ___.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses",
+    },
+    {
+        title: "Arrays in JavaScrtip can be used to store ___.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title:"String values must be enclosed within ___ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "qoutes"
+    },
+    {
+        title: "A very useful tool used during development and debugging for printing context to the debugger is:",
+        choices: ["Javascript", "terminal/bash", "for loops", "console.log"],
+        answer: "console.log"
+    },
+];
