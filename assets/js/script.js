0d5d17e74b5079142464e5bff48bcd173f281a70
aclar <a.clarke0307@gmail.com>
2021-07-16 23:43:20 -0400

    questiona and choices event

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,7 +38,7 @@ var options = [
 var totalSeconds = 80;
 var penalty = 10;
 var holdInterval = 0;
-var olNew = document.createElement("ol");
+var olNewEl = document.createElement("ol");
 
 beginEl.addEventListener("click", function(){
     if(holdInterval === 0){
@@ -54,3 +54,47 @@ beginEl.addEventListener("click", function(){
     }
     render(optionsIndex);
 });
+// questions and options function
+function render(optionsIndex) {
+    olNewEl.innerHTML = "";
+    questionsEl.innerHTML = "";
+    //for loop
+    for (var i = 0; i < options.length; i++) {
+        var clickerQuestion = options[optionsIndex].title;
+        var clickerChoices = options[optionsIndex].choices;
+        questionsEl.textContent = clickerQuestion;
+    }
+
+    clickerChoices.forEach(function(newItem){
+        var listItem = document.createElement("li");
+        listItem.textContent = newItem;
+        questionsEl.appendChild(olNewEl);
+        olNewEl.appendChild(listItem);
+        listItem.addEventListener("click", (compare));
+    })
+};
+
+function compare(event) {
+    var element = event.target
+
+    if (element.matches("li")){
+var newDiv = document.createElement("div");
+newDiv.setAttribute("id", "newDiv");
+     if(element.textContent == options[optionsIndex].answer) {
+         score++;
+         newDiv.textContent = "Correct!!";
+     }else {
+         totalSeconds =totalSeconds - penalty;
+         newDiv.textContent = "Wrong..The correct answer is" + options[optionsIndex].answer;
+
+     }
+    }
+    optionsIndex++;
+    if(optionsIndex >= options.length) {
+        allDone();
+        newDiv.textContent ="End of quiz! You got" + score + "/" + options.length + "correct!";
+    }else {
+        render(optionsIndex);
+    }
+    questionsEl.appendChild(newDiv);
+}
