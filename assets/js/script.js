4165fe64b21eb29568176e2f636cbcd0084a63c4
aclar <a.clarke0307@gmail.com>
2021-07-17 21:20:36 -0400

    finish last page

--- assets/js/script.js
+++ assets/js/script.js
@@ -73,7 +73,7 @@ function render(optionsIndex) {
         listItem.addEventListener("click", (compare));
     })
 };
-
+// Event to compare correct or incorrect choices
 function compare(event) {
     var element = event.target
 
@@ -92,9 +92,79 @@ newDiv.setAttribute("id", "newDiv");
     optionsIndex++;
     if(optionsIndex >= options.length) {
         allDone();
-        newDiv.textContent ="End of quiz! You got" + score + "/" + options.length + "correct!";
+        newDiv.textContent ="End of quiz! You got" + " " + score + "/" + options.length + "correct!";
     }else {
         render(optionsIndex);
     }
     questionsEl.appendChild(newDiv);
+};
+//last page append
+function allDone() {
+    questionsEl.innerHTML = "";
+    startTimeEl.innerHTML = "";
+
+    var newHead = document.createElement("h1");
+    newHead.setAttribute("id", "newHead");
+    newHead.textContent ="Finished!"
+    
+    questionsEl.appendChild(newHead);
+
+    var newP = document.createElement("p");
+    newP.setAttribute("id", "newP");
+
+    questionsEl.appendChild(newP);
+
+    // intials
+    var newLabel = document.createElement("label");
+    newLabel.setAttribute("id", "newLabel");
+    newLabel.textContent = "Enter Initials";
+
+    questionsEl.appendChild(newLabel);
+    // input for initials
+    var newInput = document.createElement("input");
+    newInput.setAttribute("type", "text");
+    newInput.setAttribute("id", "initials");
+    newInput.textContent = "";
+
+    questionsEl.appendChild(newInput);
+
+    //submit button
+    var newSubmit = document.createElement("button");
+    newSubmit.setAttribute("type", "submit");
+    newSubmit.setAttribute("id", "submit");
+    newSubmit.textContent ="Submit";
+
+    questionsEl.appendChild(newSubmit);
+   
+    //go back button
+    var backbutton = document.createElement("button")
+    backbutton.setAttribute("id", "back");
+    backbutton.textContent ="Try Again";
+
+    questionsEl.appendChild(backbutton);
+    //backbutton listener to begin page 
+    backbutton.addEventListener("click", function(){
+        window.location.replace("./index.html");
+    });
+
+    newSubmit.addEventListener("click", function() {
+        var initials = newInput.value;
+        if (initials === null){
+            alert("Enter initals!!");
+        }else {
+            var scoreFinal = {
+                initials: initials,
+            }
+            console.log(scoreFinal);
+            var scoreAll = localStorage.getItem("scoreAll");
+            if(scoreAll===null){
+                scoreAll =[];
+            }else{
+                scoreAll =JSON.parse(scoreAll);
+            }
+            scoreAll.push(scoreFinal);
+            var restart =JSON.stringify(scoreAll);
+            localStorage.setItem("scoreAll", restart);
+        }
+    })
 };
